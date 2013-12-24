49cb8b3bd938535cc02494b0661e412cb6bd8a9b
weyewe <w.yunnal@gmail.com>
2013-12-24 16:03:01 +0700

    initial model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XLPm::Application.config.secret_key_base = '5e67b39cb1c30cad9c424b14abcf2aee7386e71855fabfbd57be1808dd1e9608b6367bccf1a47152e8f63efb07d426101541f319d486f7dd35b856eb9a01045f'
